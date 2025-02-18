e2a332a5b07c72d16e747b7c3cb80d22b9d4d8fe
Guilherme Torres <guilhermetorres1401@gmail.com>
2025-02-18 16:25:06 -0300

    ler fichas de um diretorio especifico

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import os
+import sys
 import re
 import pandas as pd
 from PyPDF2 import PdfReader
 
-nome_arquivo_ficha = 'ficha-financeira.pdf'
-reader = PdfReader(nome_arquivo_ficha)
-paginas = reader.pages
 
 def extrair_nome_pessoa_ficha(texto: str) -> str | None:
     nome_regex = r'Nome:\s([A-Z\s]+)\s'
@@ -31,46 +30,65 @@ def remover_pontos_do_cpf(cpf: str) -> str:
     return cpf.replace('.', '').replace('-', '')
 
 
-dados = []
-for pagina in paginas:
-    texto_pagina = pagina.extract_text()
-    resultados = re.findall(r'Referência:\s([01][\d]\/[\d]{4})|(\d{3})\s([A-Z-/.\s()+0-9]+)\s(\d+,\d+)', texto_pagina)
-    dados.append(resultados)
+def main():
+    args = sys.argv
+    if len(args) < 2:
+        print('python main.py [caminho_diretorio_arquivos]')
+        sys.exit(1)
 
-lista_dados = []
-for itens_pagina in dados:
-    for item in itens_pagina:
-        lista_dados.append(item)
+    caminho_fichas = args[1]
+    diretorio_destino = os.path.join(os.getcwd(), 'planilhas')
+    if not os.path.exists(diretorio_destino):
+        os.mkdir(diretorio_destino)
+    arquivos_fichas = os.listdir(caminho_fichas)
+    for nome_arquivo_ficha in arquivos_fichas:
+        caminho_arquivo = caminho_fichas + nome_arquivo_ficha
+        reader = PdfReader(caminho_arquivo)
+        paginas = reader.pages
+        dados = []
+        for pagina in paginas:
+            texto_pagina = pagina.extract_text()
+            resultados = re.findall(r'Referência:\s([01][\d]\/[\d]{4})|(\d{3})\s([A-Z-/.\s()+0-9]+)\s(\d+,\d+)', texto_pagina)
+            dados.append(resultados)
 
-linhas = []
-referencia_atual = None
-for registro in lista_dados:
-    if registro[0]:
-        referencia_atual = registro[0]
-        continue
-    linhas.append((referencia_atual, *registro[1:]))
+        lista_dados = []
+        for itens_pagina in dados:
+            for item in itens_pagina:
+                lista_dados.append(item)
 
-colunas = ['referencia', 'codigo', 'nome', 'valor']
-df_ficha = pd.DataFrame(linhas, columns=colunas)
+        linhas = []
+        referencia_atual = None
+        for registro in lista_dados:
+            if registro[0]:
+                referencia_atual = registro[0]
+                continue
+            linhas.append((referencia_atual, *registro[1:]))
 
-# converte coluna 'valor' para numeros
-df_ficha['valor'] = df_ficha['valor'].str.replace('.', '')
-df_ficha['valor'] = pd.to_numeric(df_ficha['valor'].str.replace(',', '.'))
+        colunas = ['referencia', 'codigo', 'nome', 'valor']
+        df_ficha = pd.DataFrame(linhas, columns=colunas)
 
-codigos_vantagens = ('109', '113', '115', '127', '131', '182', '203', '119')
-soma_por_mes = df_ficha[df_ficha['codigo'].isin(codigos_vantagens)].groupby(['referencia'], sort=False, as_index=False).sum()[['referencia', 'valor']]
+        df_ficha['valor'] = df_ficha['valor'].str.replace('.', '')
+        df_ficha['valor'] = df_ficha['valor'].str.replace(',', '.').astype(float)
 
-texto_ficha_primeira_pagina = paginas[0].extract_text()
-nome_arquivo = ''
-cpf_pessoa = extrair_cpf_do_nome_arquivo_ficha(nome_arquivo_ficha)
-if cpf_pessoa is not None:
-    nome_arquivo = cpf_pessoa + '.csv'
-else:
-    cpf_pessoa = extrair_cpf_da_ficha(texto_ficha_primeira_pagina)
-    cpf_sem_pontos = remover_pontos_do_cpf(cpf_pessoa)
-    nome_arquivo = cpf_sem_pontos + '.csv'
+        codigos_vantagens = ('109', '113', '115', '127', '131', '182', '203', '119')
+        soma_por_mes = df_ficha[df_ficha['codigo'].isin(codigos_vantagens)].groupby(['referencia'], sort=False, as_index=False).sum()[['referencia', 'valor']]
+        soma_por_mes['valor'] = soma_por_mes['valor'].round(2)
 
-nome_pessoa = extrair_nome_pessoa_ficha(texto_ficha_primeira_pagina)
-soma_por_mes.columns = pd.MultiIndex.from_tuples(zip([nome_pessoa, ''], soma_por_mes.columns))
+        texto_ficha_primeira_pagina = paginas[0].extract_text()
+        nome_arquivo = ''
+        cpf_pessoa = extrair_cpf_do_nome_arquivo_ficha(nome_arquivo_ficha)
+        if cpf_pessoa is not None:
+            nome_arquivo = cpf_pessoa + '.csv'
+        else:
+            cpf_pessoa = extrair_cpf_da_ficha(texto_ficha_primeira_pagina)
+            cpf_sem_pontos = remover_pontos_do_cpf(cpf_pessoa)
+            nome_arquivo = cpf_sem_pontos + '.csv'
 
-soma_por_mes.to_csv(nome_arquivo, sep=';', index=False)
+        nome_pessoa = extrair_nome_pessoa_ficha(texto_ficha_primeira_pagina)
+        soma_por_mes.columns = pd.MultiIndex.from_tuples(zip([nome_pessoa, ''], soma_por_mes.columns))
+
+        soma_por_mes.to_csv(os.path.join(diretorio_destino, nome_arquivo), sep=';', index=False)
+
+
+if __name__ == '__main__':
+    main()
